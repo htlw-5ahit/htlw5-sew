bf1abae9e7cc184acf164fbec57b48fdac2f236b
Clemens <mail@clemens-rumpfhuber.at>
2022-03-23 00:09:37 +0100

    update 07 adapter

--- 07_adapter_buffered-reader/src/Main.java
+++ 07_adapter_buffered-reader/src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Iterator;
+
 public class Main {
 
     public static void main(String[] args) {
         try (IterableBufferedReader reader = new IterableBufferedReader("languages.txt")) {
-            for (String s : reader)
-                System.out.println(s);
+            /* for (String s : reader)
+                System.out.println(s); */
+
+            Iterator<String> i = reader.iterator();
+
+            while(i.hasNext())
+                System.out.println(i.next());
 
         } catch (Exception e) {
             e.printStackTrace();
